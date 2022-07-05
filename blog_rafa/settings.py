e50c14163b9114fd3f4b9fd2af7b585b61ca7bd0
gleysonwener <gleysonwener3@gmail.com>
2022-07-05 12:20:32 -0300

    heroku

--- blog_rafa/settings.py
+++ blog_rafa/settings.py
@@ -23,6 +23,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['blogmarisousa.herokuapp.com', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
 
 # Application definition
 
