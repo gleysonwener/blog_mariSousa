a25f1ea49ee5f90bf37775cbe926f04d749f8077
gleysonwener <gleysonwener3@gmail.com>
2022-07-05 17:58:41 -0300

    heroku

--- blog_rafa/settings.py
+++ blog_rafa/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
 import django_heroku
-
 import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
