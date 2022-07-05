b694b45d19f8712af987a0c5c972edcd28d14abf
gleysonwener <gleysonwener3@gmail.com>
2022-07-05 12:37:50 -0300

    heroku

--- blog_rafa/settings.py
+++ blog_rafa/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'blog_rafa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-default_dburl = 'postgresql:///' + os.path.join(BASE_DIR, 'postgresql')
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': config('DATABASE_URL', default=default_dburl,
 cast=dburl), }
