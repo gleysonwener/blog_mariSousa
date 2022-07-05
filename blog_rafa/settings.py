3afa0ecabd8cb492d0d35ca976b834398e2ded3f
gleysonwener <gleysonwener3@gmail.com>
2022-07-05 17:45:06 -0300

    heroku

--- blog_rafa/settings.py
+++ blog_rafa/settings.py
@@ -20,8 +20,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = ['blogmarisousa.herokuapp.com' ,'localhost', '127.0.0.1']
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
